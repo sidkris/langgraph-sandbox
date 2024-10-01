8e823ab53375a17a1ad1624f984e787a1233f79a
Siddharth Krishnan <sidkrishnan@protonmail.com>
2024-10-01 18:39:39 +0530

    reflexion

---
+++ Reflexion Agent/schemas.py
@@ -0,0 +1,16 @@
+from typing import List
+
+from langchain_core.pydantic_v1 import BaseModel, Field 
+
+class Reflection(BaseModel):
+
+    missing : str = Field(description = "critique of what is missing")
+    superflous : str = Field(description = "critique of what is superflous")
+
+
+class AnswerQuestion(BaseModel):
+
+    answer : str = Field(description = "approx. 250 word detailed answer to the question.")
+    reflection : Reflection = Field(description = "your reflection on the initial answer.")
+
+    search_queries : List[str] = Field(description = "1-3 search queries for researching improvements to address the critique of your current answer.")
